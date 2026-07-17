cb8b79778a84a50f9a09f14b3e07d1f0a8c8252f
suchi9900 <sojibvondro@gmail.com>
2026-07-17 17:30:49 +0600

    Update GitHub fork link in fork.js

--- Script/commands/fork.js
+++ Script/commands/fork.js
@@ -13,7 +13,7 @@ module.exports.run = async function({ api, event }) {
     const message = 
         "ওয়েল কাম বোকাচোদা রা, সজীবের পক্ষ থেকে স্বাগতম সবাইকে!
     
-        "🔗 GitHub Fork Link: সজীবের ইনবক্সে..!😒";
+        "🔗 GitHub Fork Link: https://github.com/suchi9900/SAJIB---2.git (সজীবের ইনবক্সে..!😒)";
      "https://i.imgur.com/ZhfQKNU.jpeg"
     return api.sendMessage(message, event.threadID, event.messageID);
 };
